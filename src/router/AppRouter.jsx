b955ea4ea659de60df3ee3dee609cd5a6a22b474
Gastón E. Acosta <gasteac@gmail.com>
2024-03-17 21:36:10 -0300

    added private and public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { HeroesRoutes  } from '../heroes/'
-import { LoginPage } from '../auth'
-import { Route, Routes } from 'react-router'
-
+import React from "react";
+import { HeroesRoutes } from "../heroes/";
+import { LoginPage } from "../auth";
+import { Route, Routes } from "react-router";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
-    <Routes>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/*' element={<HeroesRoutes/>}/>
-    </Routes>
+      <Routes>
+        {/* Rutas publicas */}
+        <Route
+          path="login/*"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        {/* Rutas privadas */}
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </>
-  )
-}
+  );
+};
